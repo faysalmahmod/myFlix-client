df73af377f9c4a4d6cc42d8b5c5bd83aa710dd2e
Faisal mahmood <faysalmahmod7@gmail.com>
2022-10-19 02:22:11 +0200

    Replacing hard coded movies data
    Hard coded movies data is replaced by the data collected in database of myFlix API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,29 +7,20 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: "Inception",
-          Description:
-            "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster",
-          ImagePath: "...",
-        },
-        {
-          _id: 2,
-          Title: "The Shawshank Redemption",
-          Description: "desc2...",
-          ImagePath: "...",
-        },
-        {
-          _id: 3,
-          Title: "Gladiator",
-          Description: "ashdgbahkl",
-          ImagePath: "...",
-        },
-      ],
+      movies: [],
       selectedMovie: null,
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://api-movie-myflix.herokuapp.com/movies")
+      .then((response) => {
+        this.setState({ movies: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -50,7 +42,7 @@ export class MainView extends React.Component {
       );
 
     if (movies.length === 0)
-      return <div className="main-view">The list is empty</div>;
+      return <div className="main-view"></div>;
 
     return (
       <div className="main-view">
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 export class MovieView extends React.Component {
+
   render() {
-    const { movie,onBackClick } = this.props;
+    const { movie, onBackClick } = this.props;
     return (
       <div className="movie-view">
         <div className="movie-poster">
@@ -15,7 +16,13 @@ export class MovieView extends React.Component {
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
-        <button onClick={()=>{onBackClick(null);}}>Back</button>
+        <button
+          onClick={() => {
+            onBackClick(null);
+          }}
+        >
+          Back
+        </button>
       </div>
     );
   }
